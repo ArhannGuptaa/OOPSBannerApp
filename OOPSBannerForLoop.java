08c27444273df8aac426a919b5cbfcf9794b7394
ArhannGuptaa <ag0215@srmist.edu.in>
2026-02-20 00:44:50 +0530

    Coded to Render OOPS as Banner using for loop

---
+++ OOPSBannerForLoop.java
@@ -0,0 +1,19 @@
+public class OOPSBannerForLoop
+{
+	public static void main()
+	{
+		String[] lines = new String[7];
+		lines[0]=String.join("  *****  "," ","  *****  "," "," ******* "," "," ********");
+		lines[1]=String.join(" ******* "," "," ******* "," ","**     **"," ","**     **");
+		lines[2]=String.join("**     **"," ","**     **"," ","**     **"," ","**       ");
+		lines[3]=String.join("**     **"," ","**     **"," ","******** "," "," ******* ");
+		lines[4]=String.join("**     **"," ","**     **"," ","**       "," ","       **");
+		lines[5]=String.join(" ******* "," "," ******* "," ","**       "," ","**     **");
+		lines[6]=String.join("  *****  "," ","  *****  "," ","**       "," ","******** ");
+		
+		for (int i=0;i<7;i++)
+		{
+			System.out.println(lines[i]);
+		}
+	}
+}
